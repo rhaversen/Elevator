4fc683000f3eff062c91ddef0c089ff532434248
Rasmus Haversen <rhaversen@gmail.com>
2025-11-19 04:08:25 +0100

    Add UMG and Slate dependencies to PublicDependencyModuleNames

--- Source/Elevator/Elevator.Build.cs
+++ Source/Elevator/Elevator.Build.cs
@@ -9,7 +9,7 @@ public class Elevator : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Json", "JsonUtilities" });
 
